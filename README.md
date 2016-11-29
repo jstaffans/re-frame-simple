36a7fba317425704f8115c9977c1ec55e6606be3
Johannes <johannes@jessica-mbp.localdomain>
2016-11-29 22:20:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+See https://github.com/Day8/re-frame/tree/master/examples/simple
---
+++ project.clj
@@ -0,0 +1,32 @@
+(defproject simple-re-frame "0.8.0"
+  :dependencies [[org.clojure/clojure        "1.8.0"]
+                 [org.clojure/clojurescript  "1.9.227"]
+                 [reagent  "0.6.0-rc"]
+                 [re-frame "0.8.0"]]
+
+  :plugins [[lein-cljsbuild "1.1.3"]
+            [lein-figwheel  "0.5.7"]]
+
+  :hooks [leiningen.cljsbuild]
+
+  :profiles {:dev {:dependencies [[figwheel "0.5.7"]]
+                   :cljsbuild
+                   {:builds {:client {:source-paths ["devsrc"]
+                                      :compiler     {:main                 "simpleexample.dev"
+                                                     :asset-path           "js"
+                                                     :optimizations        :none
+                                                     :source-map           true
+                                                     :source-map-timestamp true}}}}}
+
+             :prod {:cljsbuild
+                    {:builds {:client {:compiler {:optimizations :advanced
+                                                  :elide-asserts true
+                                                  :pretty-print  false}}}}}}
+
+  :figwheel {:repl false}
+
+  :clean-targets ^{:protect false} ["resources/public/js"]
+
+  :cljsbuild {:builds {:client {:source-paths ["src"]
+                                :compiler     {:output-dir "resources/public/js"
+                                               :output-to  "resources/public/js/client.js"}}}})
